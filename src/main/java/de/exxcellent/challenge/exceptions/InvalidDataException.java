abe125145b8978d731a6b1e50cfc97df9c6a0387
Yves Haas <yves.haas@uni-ulm.de>
2024-02-25 22:45:33 +0100

    Adds exceptions
    Adds exceptions to deal with invalid data (such
    as an empty list having no maximum) and to deal
    with data sources not being available.

---
+++ src/main/java/de/exxcellent/challenge/exceptions/InvalidDataException.java
@@ -0,0 +1,7 @@
+package de.exxcellent.challenge.exceptions;
+
+public class InvalidDataException extends Exception{
+    public InvalidDataException(String errorMessage) {
+        super(errorMessage);
+    }
+}
---
+++ src/main/java/de/exxcellent/challenge/exceptions/NoDataSourceAvailable.java
@@ -0,0 +1,9 @@
+package de.exxcellent.challenge.exceptions;
+
+public class NoDataSourceAvailable extends Exception{
+
+    public NoDataSourceAvailable(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
