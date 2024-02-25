e52dbda2dc584df64a696c990a4910ea5cb9edb9
Yves Haas <yves.haas@uni-ulm.de>
2024-02-26 00:19:22 +0100

    Updates the App.java file with the football data functionality.
    The App.java file now has the functionality to read the football data
    and print the team with the smallest difference in goals. For this
    dedicated Model, Controller and View instances for the football data
    were created and added to the app functionality.

--- src/main/java/de/exxcellent/challenge/main/App.java
+++ src/main/java/de/exxcellent/challenge/main/App.java
@@ -1,11 +1,16 @@
 package de.exxcellent.challenge.main;
 
 import de.exxcellent.challenge.controller.Controller;
+import de.exxcellent.challenge.controller.FootballDataController;
 import de.exxcellent.challenge.controller.WeatherDataController;
+import de.exxcellent.challenge.enities.FootballDataPoint;
+import de.exxcellent.challenge.enities.WeatherDataPoint;
 import de.exxcellent.challenge.exceptions.InvalidDataException;
 import de.exxcellent.challenge.exceptions.NoDataSourceAvailable;
+import de.exxcellent.challenge.model.FootballDataModel;
 import de.exxcellent.challenge.model.Model;
 import de.exxcellent.challenge.model.WeatherDataModel;
+import de.exxcellent.challenge.view.FootballDataView;
 import de.exxcellent.challenge.view.View;
 import de.exxcellent.challenge.view.WeatherDataView;
 
@@ -19,25 +24,41 @@ import java.util.List;
  */
 public final class App {
 
-    private Model weatherDataModel;
-    private Controller weatherDataController;
+    private Model<List<WeatherDataPoint>> weatherDataModel;
+    private Model<List<FootballDataPoint>> footballDataModel;
+    private Controller<Integer> weatherDataController;
+    private Controller<String> footballDataController;
     private View weatherDataView;
+    private View footballDataView;
 
-    public App(String path) {
-        //Create all necessary components (Model, View and Controller) and connect them via dependency injection
+    public App(String pathWeatherData, String pathFootballData) {
+        //Create all necessary components for the weather data functionality(Model, View and Controller) and
+        //connect them via dependency injection
         try {
-            this.weatherDataModel = new WeatherDataModel(path);
+            this.weatherDataModel = new WeatherDataModel(pathWeatherData);
         } catch (NoDataSourceAvailable e) {
-            System.err.println("Could not access data source.");
+            System.err.println("Could not access weather data source.");
             System.exit(1);
         }
         this.weatherDataController = new WeatherDataController(this.weatherDataModel);
         this.weatherDataView = new WeatherDataView(this.weatherDataController);
+
+        //Create all necessary components for the football data functionality(Model, View and Controller) and
+        //connect them via dependency injection
+        try {
+            this.footballDataModel = new FootballDataModel(pathFootballData);
+        } catch (NoDataSourceAvailable e) {
+            System.err.println("Could not access football data source.");
+            System.exit(1);
+        }
+        this.footballDataController = new FootballDataController(this.footballDataModel);
+        this.footballDataView = new FootballDataView(this.footballDataController);
     }
 
     public static void main(String... args) {
         //Instantiate the app and run the app logic
-        App app = new App("src/main/resources/de/exxcellent/challenge/weather.csv");
+        App app = new App("src/main/resources/de/exxcellent/challenge/weather.csv",
+        "src/main/resources/de/exxcellent/challenge/football.csv");
         app.run();
     }
 
@@ -47,6 +68,7 @@ public final class App {
     private void run() {
         try {
             this.weatherDataView.printData();
+            this.footballDataView.printData();
         } catch (InvalidDataException e) {
             System.err.println("Data has invalid format.");
             System.exit(2);
