4e54e40b4ba28e628a09ab4d71f2abe502ec4b53
Yves Haas <yves.haas@uni-ulm.de>
2024-02-25 23:06:32 +0100

    Adds tests for the WeatherDataController
    Adds two tests fot the WeatherDataController. One
    test tests if the maximum is found correctly and
    one test checks if the controller handles missing
    data correctly.

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,31 +1,84 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.controller.Controller;
+import de.exxcellent.challenge.controller.WeatherDataController;
+import de.exxcellent.challenge.enities.WeatherDataPoint;
+import de.exxcellent.challenge.exceptions.InvalidDataException;
+import de.exxcellent.challenge.model.Model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Example JUnit 5 test case.
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
+    private Model mockBackend1;
+    private Model mockBackend2;
 
-    private String successLabel = "not successful";
+    private Controller controller1;
+
+    private Controller controller2;
+
+
 
     @BeforeEach
     void setUp() {
-        successLabel = "successful";
+        //Setup a Model for to test if the controller finds the day with the maximum temperature spread
+         this.mockBackend1 = new Model() {
+            @Override
+            public List<WeatherDataPoint> getWeatherData() {
+                return new ArrayList<>(Arrays.asList(
+                        new WeatherDataPoint(1,30,20),
+                        new WeatherDataPoint(2,25,20),
+                        new WeatherDataPoint(3,25,5),
+                        new WeatherDataPoint(4,40,10),
+                        new WeatherDataPoint(5,20,25),
+                        new WeatherDataPoint(6,25,20)
+                ));
+            }
+        };
+
+         //Setup a Model to test if the controller deals with no available data correctly
+        this.mockBackend2 = new Model() {
+            @Override
+            public List<WeatherDataPoint> getWeatherData() {
+                return new ArrayList<>();
+            }
+        };
+
+        this.controller1 = new WeatherDataController(this.mockBackend1);
+        this.controller2 = new WeatherDataController(this.mockBackend2);
+
+
     }
 
+    /**
+     * Test to see if the controller correctly deals with no available data by throwing a InvalidDataException
+     */
     @Test
-    void aPointlessTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
+    void testMaximumTempSpreadOfEmptyArray() {
+        assertThrows(InvalidDataException.class, () -> {this.controller2.getDayWithMaxTempSpread();});
     }
 
+    /**
+     * Test to see if the controller correctly finds the number of the day with the maximum temperature spread
+     * even if that data includes cases where MnT > MxT
+     */
     @Test
-    void runFootball() {
-        App.main("--football", "football.csv");
+    void testMaximumTempSpreadOfArray() {
+        try {
+            assertEquals(4, this.controller1.getDayWithMaxTempSpread());
+        } catch (InvalidDataException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
