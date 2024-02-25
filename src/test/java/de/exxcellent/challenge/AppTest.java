7f7f2f7b5bd08eec4460b8eed5644a0516babd0f
Yves Haas <yves.haas@uni-ulm.de>
2024-02-26 00:21:12 +0100

    Adds test for the football data functionality of the app
    Adds two tests football data functionality of the
    app. One test tests whether the controller
    correctly deals with missing data and another test
    tests whether the name of the team with the
    minimum absolute goal difference is returned.

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,7 +1,9 @@
 package de.exxcellent.challenge;
 
 import de.exxcellent.challenge.controller.Controller;
+import de.exxcellent.challenge.controller.FootballDataController;
 import de.exxcellent.challenge.controller.WeatherDataController;
+import de.exxcellent.challenge.enities.FootballDataPoint;
 import de.exxcellent.challenge.enities.WeatherDataPoint;
 import de.exxcellent.challenge.exceptions.InvalidDataException;
 import de.exxcellent.challenge.model.Model;
@@ -20,62 +22,107 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
-    private Model mockBackend1;
-    private Model mockBackend2;
 
-    private Controller controller1;
 
-    private Controller controller2;
+    private Controller<Integer> weatherController1;
+    private Controller<Integer> weatherController2;
+    private Controller<String> footballController1;
+    private Controller<String> footballController2;
+
 
 
 
     @BeforeEach
     void setUp() {
-        //Setup a Model for to test if the controller finds the day with the maximum temperature spread
-         this.mockBackend1 = new Model<List<WeatherDataPoint>>() {
+        //Setup a weather model for to test if the weather controller finds the day with the maximum temperature spread
+        Model<List<WeatherDataPoint>> weatherMockBackend1 = new Model<List<WeatherDataPoint>>() {
             @Override
             public List<WeatherDataPoint> getData() {
                 return new ArrayList<>(Arrays.asList(
-                        new WeatherDataPoint(1,30,20),
-                        new WeatherDataPoint(2,25,20),
-                        new WeatherDataPoint(3,25,5),
-                        new WeatherDataPoint(4,40,10),
-                        new WeatherDataPoint(5,20,25),
-                        new WeatherDataPoint(6,25,20)
+                        new WeatherDataPoint(1, 30, 20),
+                        new WeatherDataPoint(2, 25, 20),
+                        new WeatherDataPoint(3, 25, 5),
+                        new WeatherDataPoint(4, 40, 10),
+                        new WeatherDataPoint(5, 20, 25),
+                        new WeatherDataPoint(6, 25, 20)
                 ));
             }
         };
 
-         //Setup a Model to test if the controller deals with no available data correctly
-        this.mockBackend2 = new Model<List<WeatherDataPoint>>() {
+         //Setup a weather model to test if the weather controller deals with no available data correctly
+        Model<List<WeatherDataPoint>> weatherMockBackend2 = new Model<List<WeatherDataPoint>>() {
             @Override
             public List<WeatherDataPoint> getData() {
                 return new ArrayList<>();
             }
         };
 
-        this.controller1 = new WeatherDataController(this.mockBackend1);
-        this.controller2 = new WeatherDataController(this.mockBackend2);
+        this.weatherController1 = new WeatherDataController(weatherMockBackend1);
+        this.weatherController2 = new WeatherDataController(weatherMockBackend2);
 
+
+        //Setup a football model for to test if the football controller finds the team with the lowest goal difference
+        Model<List<FootballDataPoint>> footballMockBackend1 = new Model<List<FootballDataPoint>>() {
+            @Override
+            public List<FootballDataPoint> getData() {
+                return new ArrayList<>(Arrays.asList(
+                        new FootballDataPoint("a", 120, 100),
+                        new FootballDataPoint("b", 100, 110),
+                        new FootballDataPoint("c", 180, 100),
+                        new FootballDataPoint("d", 200, 100)
+                ));
+            }
+        };
+
+        //Setup a football model to test if the football controller deals with no available data correctly
+        Model<List<FootballDataPoint>> footballMockBackend2 = new Model<List<FootballDataPoint>>() {
+            @Override
+            public List<FootballDataPoint> getData() {
+                return new ArrayList<>();
+            }
+        };
+
+        this.footballController1 = new FootballDataController(footballMockBackend1);
+        this.footballController2 = new FootballDataController(footballMockBackend2);
 
     }
 
     /**
-     * Test to see if the controller correctly deals with no available data by throwing a InvalidDataException
+     * Test to see if the weather controller correctly deals with no available data by throwing a InvalidDataException
      */
     @Test
     void testMaximumTempSpreadOfEmptyArray() {
-        assertThrows(InvalidDataException.class, () -> {this.controller2.calculate();});
+        assertThrows(InvalidDataException.class, () -> {this.weatherController2.calculate();});
     }
 
     /**
-     * Test to see if the controller correctly finds the number of the day with the maximum temperature spread
+     * Test to see if the weather controller correctly finds the number of the day with the maximum temperature spread
      * even if that data includes cases where MnT > MxT
      */
     @Test
     void testMaximumTempSpreadOfArray() {
         try {
-            assertEquals(4, this.controller1.calculate());
+            assertEquals(4, this.weatherController1.calculate());
+        } catch (InvalidDataException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Test to see if the football controller correctly deals with no available data by throwing a InvalidDataException
+     */
+    @Test
+    void testMinimumGoalDifferenceEmptyArray() {
+        assertThrows(InvalidDataException.class, () -> {this.footballController2.calculate();});
+    }
+
+    /**
+     * Test to see if the football controller correctly finds the team with the lowest absolute goal difference.
+     */
+    @Test
+    void testMinimumGoalDifference() {
+        try {
+            assertEquals("b", this.footballController1.calculate());
         } catch (InvalidDataException e) {
             throw new RuntimeException(e);
         }
